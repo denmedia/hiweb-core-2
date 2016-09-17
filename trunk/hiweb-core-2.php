0421fcd57792bbe931956d794658fe7f8ad58565
DenMedia <d9251132826@gmail.com>
2016-09-17 22:11:32 +0300

    2.2.1.1

--- trunk/hiweb-core-2.php
+++ trunk/hiweb-core-2.php
@@ -3,7 +3,7 @@
 	Plugin Name: hiWeb Core 2
 	Plugin URI: http://plugins.hiweb.moscow/core
 	Description: Special framework plugin
-	Version: 2.2.1.0
+	Version: 2.2.1.1
 	Author: Den Media
 	Author URI: http://hiweb.moscow
 	*/
--- trunk/modules/console.php
+++ trunk/modules/console.php
@@ -7,7 +7,7 @@
 	 * Date: 30.06.2016
 	 * Time: 17:26
 	 */
-	class hiweb_console {
+	class hiweb_console{
 
 		public $_infos = array();
 		public $_warns = array();
@@ -16,109 +16,98 @@
 		public $_mess = array();
 
 
-		public function __construct( $info = null ) {
-			if ( ! is_null( $info ) ) {
+		public function __construct( $info = null ){
+			if( !is_null( $info ) ){
 				$this->info( $info );
 			}
-			add_action( 'shutdown', array( $this, 'echo_footer' ), 10 );
+			if( !hiweb()->wp()->is_ajax() )
+				add_action( 'shutdown', array( $this, 'echo_footer' ), 10 );
 		}
+
 
 		/**
 		 * Вывести информацию в консоль
-		 *
 		 * @param $info - информация
 		 * @param bool $debugMod - дополнительная информация
 		 */
-		public function info( $info, $debugMod = false ) {
+		public function info( $info, $debugMod = false ){
 			//check call from __construct function
-			$deb               = debug_backtrace();
-			$function          = isset( $deb[1]['function'] ) ? $deb[1]['function'] : '';
-			$class             = isset( $deb[1]['class'] ) ? $deb[1]['class'] : '';
+			$deb = debug_backtrace();
+			$function = isset( $deb[1]['function'] ) ? $deb[1]['function'] : '';
+			$class = isset( $deb[1]['class'] ) ? $deb[1]['class'] : '';
 			$callFromConstruct = $function == '__construct' && $class == 'hiweb_console';
 			//
-			$this->_infos[]       = $info;
+			$this->_infos[] = $info;
 			$this->mess['info'][] = $info;
-			$this->_mess[]        = array(
-				'data'      => $info,
-				'type'      => 'info',
-				'debug'     => $debugMod,
-				'microtime' => microtime( 1 ),
-				'file'      => hiweb()->backtrace()->file_locate( $callFromConstruct ? 3 : 2 ),
-				'function'  => hiweb()->backtrace()->function_trace( $callFromConstruct ? 3 : 2 )
+			$this->_mess[] = array(
+				'data' => $info, 'type' => 'info', 'debug' => $debugMod, 'microtime' => microtime( 1 ), 'file' => hiweb()->backtrace()->file_locate( $callFromConstruct ? 3 : 2 ),
+				'function' => hiweb()->backtrace()->function_trace( $callFromConstruct ? 3 : 2 )
 			);
 		}
+
 
 		/**
 		 * Вывести предупреждение в консоль
-		 *
 		 * @param $info - информация
 		 * @param bool $debugMod - дополнительная информация
 		 */
-		public function warn( $info, $debugMod = false ) {
-			$this->_warns[]       = $info;
+		public function warn( $info, $debugMod = false ){
+			$this->_warns[] = $info;
 			$this->mess['warn'][] = $info;
-			$this->_mess[]        = array(
-				'data'      => $info,
-				'type'      => 'warn',
-				'debug'     => $debugMod,
-				'microtime' => microtime( 1 ),
-				'file'      => hiweb()->backtrace()->file_locate( 2 ),
-				'function'  => hiweb()->backtrace()->function_trace( 2 )
+			$this->_mess[] = array(
+				'data' => $info, 'type' => 'warn', 'debug' => $debugMod, 'microtime' => microtime( 1 ), 'file' => hiweb()->backtrace()->file_locate( 2 ), 'function' => hiweb()->backtrace()->function_trace( 2 )
 			);
 		}
+
 
 		/**
 		 * Вывести в консоль ошибку
-		 *
 		 * @param $info - информация
 		 * @param bool $debugMod - дополнительная информация
-		 *
 		 * @version 1.1
 		 */
-		public function error( $info, $debugMod = false ) {
-			$this->_errors[]       = $info;
+		public function error( $info, $debugMod = false ){
+			$this->_errors[] = $info;
 			$this->mess['error'][] = $info;
-			$this->_mess[]         = array(
-				'data'      => $info,
-				'type'      => 'error',
-				'debug'     => $debugMod,
-				'microtime' => microtime( 1 ),
-				'file'      => hiweb()->backtrace()->file_locate( 2 ),
-				'function'  => hiweb()->backtrace()->function_trace( 2 )
+			$this->_mess[] = array(
+				'data' => $info, 'type' => 'error', 'debug' => $debugMod, 'microtime' => microtime( 1 ), 'file' => hiweb()->backtrace()->file_locate( 2 ), 'function' => hiweb()->backtrace()->function_trace( 2 )
 			);
 		}
 
+
 		/**
 		 * Форс вывод console.info($info)
-		 *
 		 * @param $info
 		 */
-		public function echo_info( $info ) {
+		public function echo_info( $info ){
 			echo 'console.info(' . json_encode( $info ) . ');';
 		}
 
+
 		/**
 		 * Форс вывод console.warn($info)
-		 *
 		 * @param $info
 		 */
-		public function echo_warn( $info ) {
+		public function echo_warn( $info ){
 			echo 'console.warn(' . json_encode( $info ) . ');';
 		}
 
+
 		/**
 		 * Форс вывод console.error($info)
-		 *
 		 * @param $info
 		 */
-		public function echo_error( $info ) {
+		public function echo_error( $info ){
 			echo 'console.error(' . json_encode( $info ) . ');';
 		}
 
-		public function echo_footer() {
+
+		public function echo_footer(){
 			echo '<script>';
-			foreach ( $this->_mess as $info ) {
-				$this->{'echo_' . $info['type']}( $info['debug'] ? ( is_array( $info['data'] ) ? array_merge( array( '► ' . $info['function'], '► ' . $info['file'] ), $info['data'] ) : $info['function'] . '() : ' . $info['data'] . chr( 13 ) . chr( 10 ) . '► ' . $info['file'] ) : $info['data'] );
+			foreach( $this->_mess as $info ){
+				$this->{'echo_' . $info['type']}( $info['debug'] ? ( is_array( $info['data'] ) ? array_merge( array(
+					'► ' . $info['function'], '► ' . $info['file']
+				), $info['data'] ) : $info['function'] . '() : ' . $info['data'] . chr( 13 ) . chr( 10 ) . '► ' . $info['file'] ) : $info['data'] );
 			}
 			echo '</script>';
 		}
