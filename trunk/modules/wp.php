f0c8bbe6504afececa1029641c90b2beb13e9eff
DenMedia <d9251132826@gmail.com>
2016-09-22 13:17:07 +0300

    2.4.0.0

--- trunk/modules/wp.php
+++ trunk/modules/wp.php
@@ -687,14 +687,24 @@
 
 		private $name;
 		private $labels;
-		private $hierarchical;
+		private $description;
 		private $public;
+		private $publicly_queryable;
+		private $hierarchical;
 		private $show_ui;
-		private $show_admin_column;
+		private $show_in_menu;
 		private $show_in_nav_menus;
 		private $show_tagcloud;
+		private $show_in_quick_edit;
+		private $show_admin_column;
+		private $meta_box_cb;
+		private $capabilities;
+		private $rewrite;
+		private $update_count_callback;
+		private $_builtin;
 		private $defaults = array(
-			'labels' => array(), 'hierarchical' => false, 'public' => true, 'show_ui' => true, 'show_admin_column' => true, 'show_in_nav_menus' => true, 'show_tagcloud' => true
+			'labels' => array(), 'description' => '', 'public' => true, 'publicly_queryable' => null, 'hierarchical' => false, 'show_ui' => null, 'show_in_menu' => null, 'show_in_nav_menus' => null, 'show_tagcloud' => null, 'show_in_quick_edit' => null,
+			'show_admin_column' => false, 'meta_box_cb' => null, 'capabilities' => array(), 'rewrite' => true, 'update_count_callback' => '', '_builtin' => false,
 		);
 		private $object_type = array();
 
@@ -767,11 +777,147 @@
 		 */
 		public function labels( $labels = null ){
 			if( !is_null( $labels ) ){
-				if(!is_array($labels)) $labels = array('name' => $labels);
+				if( !is_array( $labels ) )
+					$labels = array( 'name' => $labels );
 				$this->labels = $labels;
 				return $this;
 			}
 			return $this->labels;
+		}
+
+
+		/**
+		 * @param null $description
+		 * @return hw_wp_add_taxonomy|string
+		 */
+		public function description( $description = null ){
+			if( !is_null( $description ) ){
+				if( !is_array( $description ) )
+					$description = array( 'name' => $description );
+				$this->description = $description;
+				return $this;
+			}
+			return $this->description;
+		}
+
+
+		/**
+		 * @param null $publicly_queryable
+		 * @return hw_wp_add_taxonomy|string
+		 */
+		public function publicly_queryable( $publicly_queryable = null ){
+			if( !is_null( $publicly_queryable ) ){
+				if( !is_array( $publicly_queryable ) )
+					$publicly_queryable = array( 'name' => $publicly_queryable );
+				$this->publicly_queryable = $publicly_queryable;
+				return $this;
+			}
+			return $this->publicly_queryable;
+		}
+
+
+		/**
+		 * @param null $show_in_menu
+		 * @return hw_wp_add_taxonomy|string
+		 */
+		public function show_in_menu( $show_in_menu = null ){
+			if( !is_null( $show_in_menu ) ){
+				if( !is_array( $show_in_menu ) )
+					$show_in_menu = array( 'name' => $show_in_menu );
+				$this->show_in_menu = $show_in_menu;
+				return $this;
+			}
+			return $this->show_in_menu;
+		}
+
+
+		/**
+		 * @param null $show_in_quick_edit
+		 * @return hw_wp_add_taxonomy|string
+		 */
+		public function show_in_quick_edit( $show_in_quick_edit = null ){
+			if( !is_null( $show_in_quick_edit ) ){
+				if( !is_array( $show_in_quick_edit ) )
+					$show_in_quick_edit = array( 'name' => $show_in_quick_edit );
+				$this->show_in_quick_edit = $show_in_quick_edit;
+				return $this;
+			}
+			return $this->show_in_quick_edit;
+		}
+
+
+		/**
+		 * @param null $meta_box_cb
+		 * @return hw_wp_add_taxonomy|string
+		 */
+		public function meta_box_cb( $meta_box_cb = null ){
+			if( !is_null( $meta_box_cb ) ){
+				if( !is_array( $meta_box_cb ) )
+					$meta_box_cb = array( 'name' => $meta_box_cb );
+				$this->meta_box_cb = $meta_box_cb;
+				return $this;
+			}
+			return $this->meta_box_cb;
+		}
+
+
+		/**
+		 * @param null $capabilities
+		 * @return hw_wp_add_taxonomy|string
+		 */
+		public function capabilities( $capabilities = null ){
+			if( !is_null( $capabilities ) ){
+				if( !is_array( $capabilities ) )
+					$capabilities = array( 'name' => $capabilities );
+				$this->capabilities = $capabilities;
+				return $this;
+			}
+			return $this->capabilities;
+		}
+
+
+		/**
+		 * @param null $rewrite
+		 * @return hw_wp_add_taxonomy|string
+		 */
+		public function rewrite( $rewrite = null ){
+			if( !is_null( $rewrite ) ){
+				if( !is_array( $rewrite ) )
+					$rewrite = array( 'name' => $rewrite );
+				$this->rewrite = $rewrite;
+				return $this;
+			}
+			return $this->rewrite;
+		}
+
+
+		/**
+		 * @param null $update_count_callback
+		 * @return hw_wp_add_taxonomy|string
+		 */
+		public function update_count_callback( $update_count_callback = null ){
+			if( !is_null( $update_count_callback ) ){
+				if( !is_array( $update_count_callback ) )
+					$update_count_callback = array( 'name' => $update_count_callback );
+				$this->update_count_callback = $update_count_callback;
+				return $this;
+			}
+			return $this->update_count_callback;
+		}
+
+
+		/**
+		 * @param null $_builtin
+		 * @return hw_wp_add_taxonomy|string
+		 */
+		public function _builtin( $_builtin = null ){
+			if( !is_null( $_builtin ) ){
+				if( !is_array( $_builtin ) )
+					$_builtin = array( 'name' => $_builtin );
+				$this->_builtin = $_builtin;
+				return $this;
+			}
+			return $this->_builtin;
 		}
 
 
